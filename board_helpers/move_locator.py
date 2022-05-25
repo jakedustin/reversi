d7fb2ee1e4ee1647fcbda44805f986caac2b1e1c
Jake Dustin <jdustin@qualtrics.com>
2022-05-25 11:29:42 -0600

    initial commit

---
+++ board_helpers/move_locator.py
@@ -0,0 +1,10 @@
+
+class MoveLocator:
+
+    def is_edge(self, move): 
+        if move[0] == 0 or move[0] == 7:
+            print('move: ', str(move))
+            return move[1] < 6 and move[1] > 1
+        elif (move[1] == 0 or move[1] == 7):
+            print('move: ', str(move))
+            return move[0] < 6 and move[0] > 1
