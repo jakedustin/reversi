6e6613ac41c4ce232ccdc10b6af7617e8fe04c82
Jake Dustin <jdustin@qualtrics.com>
2022-05-25 11:43:42 -0600

    added is_corner

--- board_helpers/move_locator.py
+++ board_helpers/move_locator.py
@@ -8,3 +8,9 @@ class MoveLocator:
         elif (move[1] == 0 or move[1] == 7):
             print('move: ', str(move))
             return move[0] < 6 and move[0] > 1
+
+    def is_corner(self, move):
+        if move[0] == 0:
+            return move[1] == 0 or move[1] == 7
+        elif move[0] == 7:
+            return move[1] == 0 or move[1] == 7
