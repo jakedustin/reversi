5862d111fbb8c21c1e50f27a31ba326d9d127451
Moa Dijkwel <moadijkwel@Moas-MacBook-Air.local>
2022-05-27 15:55:51 -0600

    changed alpha beta

--- reversi_bot.py
+++ reversi_bot.py
@@ -108,38 +108,32 @@ class ReversiBot:
         if depth == self.max_depth:
             # TODO: calculate value of state
             value_state = vc.ValueCalculator.calculate_state_utility(state, self.utility, state.turn)
+            return value_state
             # if generated value > value from parent, return value, moves_taken
-            if value_state > value:
-                return [value_state, moves_taken]
-            # else return +-inf, moves_taken
-            return [float('inf'), moves_taken]
+            # if value_state > value:
+            #     return [value_state, moves_taken]
+            # # else return +-inf, moves_taken
+            # return [float('inf'), moves_taken]
 
-        # for move in valid_moves
-        for i in range(len(moves_taken), len(valid_moves)):
-            left_state = valid_moves[i * 2]
-            right_state = valid_moves[i * 2 + 1]
-            left_result = self.alpha_beta(left_state, depth + 1, value, moves_taken.append[left_state], alpha,beta)
-            right_result = self.alpha_beta(right_state, depth + 1, value, moves_taken.append[right_state],alpha,beta)
-
-            # maximize
-            if depth % 2 == 0:
+        # maximize
+        if depth % 2 == 0:
+            value = -float('inf')
+            # for move in valid_moves
+            for state in valid_moves:
+                value = max(value, self.alpha_beta(state, depth + 1, value, moves_taken.append(state), alpha,beta))
+                # return [value, moves_taken]
                 alpha = max(alpha, value_state)
-                if alpha >= beta:
-                        break
-                if left_result[0] > right_result[0]:
-                    return left_result
-                else:
-                    return right_result
-            # minimize
-            else:
+                if beta <= alpha:
+                    break
+                return [value, moves_taken]
+        else:
+            value = float('inf')
+            for state in valid_moves:
+                value = min(value, self.alpha_beta(state, depth + 1, value, moves_taken.append(state), alpha, beta))
                 beta = min(beta, value_state)
                 if beta <= alpha:
                     break
-                if left_result[0] > right_result[0]:
-                    return right_result
-                else:
-                    return left_result
-
+                return [value, moves_taken]
 
         # maximize
         # if depth % 2 == 0:
